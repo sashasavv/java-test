e7b0372ae2f24add82c4ab526297458a7b32208f
OLeksandr <savchykhin@gmail.com>
2024-08-08 19:08:49 -0700

    handle driver

--- src/test/java/com/example/TestController.java
+++ src/test/java/com/example/TestController.java
@@ -10,15 +10,22 @@ public class TestController {
 
     @BeforeClass(alwaysRun = true)
     public void setUp() {
-        String driverPath = "/usr/local/bin/geckodriver";
-        System.setProperty("webdriver.gecko.driver", driverPath);
-        driver = new FirefoxDriver();
-        driver.get("http://localhost:8000");
+        try {
+            String driverPath = "/usr/local/bin/geckodriver";
+            System.setProperty("webdriver.gecko.driver", driverPath);
+            driver = new FirefoxDriver();
+        } catch (Exception e) {
+            System.err.println("Failed to initialize WebDriver: " + e.getMessage());
+        }
+        driver.get("http://127.0.0.1:8000");
+
     }
 
     @AfterClass(alwaysRun = true)
     public void closeDriver() {
-        driver.close();
+        if (driver != null) {
+            driver.close();
+        }
     }
 }
 
