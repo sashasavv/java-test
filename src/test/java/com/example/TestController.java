941059c087c17c70a7fe26e9cc367f2184e9c42e
OLeksandr <savchykhin@gmail.com>
2024-08-08 20:09:50 -0700

    clean up

--- src/test/java/com/example/TestController.java
+++ src/test/java/com/example/TestController.java
@@ -6,35 +6,19 @@ import org.testng.annotations.BeforeClass;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 public class TestController {
     protected WebDriver driver;
 
     @BeforeClass(alwaysRun = true)
     public void setUp() {
-        try {
-            // System.out.println("Project Directory: " + System.getProperty("user.dir"));
-            String driverPath ="/usr/local//bin/geckodriver";
-        
-        // Check if the file exists
-        File geckoDriverFile = new File(driverPath);
-        if (!geckoDriverFile.exists()) {
-            throw new RuntimeException("Geckodriver file does not exist at path: " + driverPath);
-        }
-        // Set Firefox options to run in headless mode
+        String driverPath = "/usr/local/bin/geckodriver";
+
         FirefoxOptions options = new FirefoxOptions();
         options.setHeadless(true);
-        options.addArguments("--no-sandbox"); // Disable sandboxing
+        options.addArguments("--no-sandbox");
         System.setProperty("webdriver.gecko.driver", driverPath);
         driver = new FirefoxDriver(options);
-        } catch (Exception e) {
-            System.err.println("Failed to initialize WebDriver: " + e.getMessage());
-            e.printStackTrace();
-        }
+
         driver.get("http://127.0.0.1:8000");
     }
 
